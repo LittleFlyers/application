47b9bc389f79186d22c34c18c6736972dc064c7e
LittleFlyer <2538285284@qq.com>
2018-04-03 17:14:44 +0800

    ceshi

--- knowledge/controller/Paper.php
+++ knowledge/controller/Paper.php
@@ -64,7 +64,37 @@ class Paper
  
         $result['err_code'] = 0;
   //      $result['paper'] = $paper_list;
-        $result['question_id'] = $paper_qs_id;
+        $result['paper_data'] = $paper_list;
+        $result['paper_question'] = $question_list;
+       // $result['img'] = $question_img_list;
+       // $result['answer'] = $question_answer;
+
+        return json_encode($result);
+    }
+
+    public function oneAnswerH()
+    {
+        $paper_id = input('post.paper_id');
+        $P = db('paper');
+        $PQ = db('paper_question');
+        $Q = db('bank_question');
+        $A = db('bank_question_answer');
+
+        $check['paper_id'] = $paper_id;
+
+        $paper_list = $P -> where($check) -> select();
+        $paper_question_id = $PQ -> field('question_id') -> where($check) -> select();
+        $paper_qs_id = [];
+        foreach($paper_question_id as $value){
+            array_push($paper_qs_id,$value['question_id']);
+        }
+        $question_list = $Q -> where('question_id', 'IN', $paper_qs_id) -> select();
+     //   $question_img_list = $Q -> where('question_id', 'IN', $paper_question_id) -> select();
+       // $question_answer = $A -> where($check) -> select();
+ 
+        $result['err_code'] = 0;
+  //      $result['paper'] = $paper_list;
+        $result['paper_data'] = $paper_list;
         $result['paper_question'] = $question_list;
        // $result['img'] = $question_img_list;
        // $result['answer'] = $question_answer;
